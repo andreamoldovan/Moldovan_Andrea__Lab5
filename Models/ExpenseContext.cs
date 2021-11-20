2a4c402ad21497e44b544d59e55916ed405330c3
Andrea Moldovan <andreamoldovan98@yahoo.com>
2021-11-20 18:09:53 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Moldovan_Andrea__Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+
+    }
+}
